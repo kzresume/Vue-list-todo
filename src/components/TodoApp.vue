da2f86d55ca1073e73a54dc90401178ecc6c48c6
kzresume <inz.zawisza.karol@gmail.com>
2021-07-26 00:07:50 +0200

    Pierwsze

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Vue todo app</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
+ 
